ad409ccf10fa631ff45b94ee44bd34fa60af1c35
Voctor <voctorbidoof@gmail.com>
2021-11-14 13:54:07 -0800

    ssl redirect hotfix

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const sslRedirect =  require('heroku-ssl-redirect');
+const sslRedirect =  require('heroku-ssl-redirect').default;
 const express = require('express')
 const path = require('path');
 
