7f3ea6a2988b4ddbf23c1593a137b715f553f087
Lincoln <Lsha0730@gmail.com>
2021-11-14 14:01:23 -0800

    Webcam + Screenshot Loop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from "react"
+import Webcam from "react-webcam";
+import {WebcamCapture} from './Webcam.jsx';
+
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="UI-container">
+        <div className="keyInstr-container">
+          <p>Hello</p>
+        </div>
+
+        <WebcamCapture />
+
+
+        <div className="subtitles-container">
+          <p>The University of British Columbia</p>
+        </div>
+      </div>
     </div>
   );
 }
