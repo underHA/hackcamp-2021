181add1f68f261376b986c7107fa3132614c2f98
Voctor <voctorbidoof@gmail.com>
2021-11-14 12:35:17 -0800

    trying forcing SSL

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+const sslRedirect =  require('heroku-ssl-redirect').sslRedirect;
 const express = require('express')
 const path = require('path');
 
 const app = express()
 const port = process.env.PORT || 3000 // Heroku will need the PORT environment variable
 
+app.use(sslRedirect());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(port, () => console.log(`App is live on port ${port}!`))
